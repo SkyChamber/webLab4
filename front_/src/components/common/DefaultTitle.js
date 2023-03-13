240774c9f8cf3ecc7cab7989db121bd1b6926f49
SkyChamber <48603197+SkyChamber@users.noreply.github.com>
2023-03-13 16:51:47 +0300

    Add files via upload

---
+++ front_/src/components/common/DefaultTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class DefaultTitle extends React.Component{
+    render() {
+        return (
+            <div className={"text-center"}>
+                <p>Мустафин Родион Андреевич, P32301<br/>Вариант 66676</p>
+            </div>
+        )
+    }
+}
+
+export default DefaultTitle;
